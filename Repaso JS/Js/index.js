3ddd5a7b2232fd421b0b43fe482d801071e4e89e
NELSON GERARDO FERNANDEZ GONZALEZ <nfernandezg@ucenfotec.ac.cr>
2026-02-05 20:53:39 -0600

    new:html,js,css

---
+++ Repaso JS/Js/index.js
@@ -0,0 +1,115 @@
+let txtPeso = document.getElementById("txtPeso");
+let txtEstatura = document.querySelector("#txtEstatura");
+
+let btnCalcularIMC = document.querySelector("#btnCalcularIMC")
+
+let parrafoResultado = document.querySelector("#sctResultado p");
+
+/* Forma incorrecto
+var nombreGato = "Stella"; 
+
+let nombrePerro = "Mia";  */
+
+
+// Forma correcta
+let nombrePerro = "Mia";
+
+let nombreGato = "Stella";
+
+let nombreCabra;
+nombreCabra = "Amber";
+nombreCabra = "Amatista";
+
+
+// Constantes
+const PI = 3.14;
+console.log(PI);
+
+const MAXIMO_USUARIOS = 1000;
+
+
+// Funcion 
+/* Crear una función para calcular el IMC con la siguiente fórmula:
+IMC = peso / estatura^2
+Mostrar en la consola un mensaje que diga "El IMC es: --"
+Datos de prueba en kg       m       IMC
+                    80      1.7     27.7
+                    60      1.8     18.5
+*/
+
+function calcularIMC() {
+
+    let peso = txtPeso.value;
+    let estatura = txtEstatura.value;
+
+
+    if(validarCamposVacios() === false) {
+        let imc = peso / Math.pow(estatura, 2); // estatura * estura
+        //console.log("El IMC es: " + imc);
+        parrafoResultado.innerText = imc.toFixed(2);
+    } else{
+        Swal.fire({
+            title: "No se pudo calcular el IMC",
+            text: "Por favor revise los campos resaltados",
+            icon: "error",
+            confirmButtonText: "Aceptar"
+        });
+    }
+}
+
+
+function validarCamposVacios(){
+    let error = false;  //iniciamos por defecto en false asumiendo que no hay errores
+
+    if(txtPeso.value === ""){
+        txtPeso.classList.add("input-error");
+        error = true;
+    }else{
+        txtPeso.classList.remove("input-error");
+    }
+
+    if(txtEstatura.value === ""){
+        txtEstatura.classList.add("input-error");
+        error = true;
+    }else{
+        txtEstatura.classList.remove("input-error");
+    }
+
+    return error;
+}
+
+
+
+btnCalcularIMC.addEventListener("click", calcularIMC);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function CelsiusFahrenheit(F){
+    C=(F-32)/1.8
+    console.log("El valor: " +F+ " convertido a Fahrenheit es igual a: " +C);
+}
+
+CelsiusFahrenheit(32); */
