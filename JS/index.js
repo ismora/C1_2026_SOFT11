e4ece7f036f69920d3a0089f59807e30ad9fff26
Maycol Porras <maycolporras09@icloud.com>
2026-02-05 19:45:37 -0600

    [fixed]: Password funct

--- JS/index.js
+++ JS/index.js
@@ -24,70 +24,71 @@ function validarFormulario() {
                 text: "Las contraseñas no coinciden. Por favor, inténtelo de nuevo.",
                 confirmButtonText: "Entendido",
             });
+            if (contraseña.value !== confirmarContraseña.value) {
+                contraseña.classList.add("input-error");
+                confirmarContraseña.classList.add("input-error");
+                error = true;
+            } else {
+                contraseña.classList.remove("input-error");
+                confirmarContraseña.classList.remove("input-error");
+            }
         }
-    }else {
-            Swal.fire({
-                icon: "error",
-                title: "Espacios en blanco",
-                text: "Por favor revise los campos resaltados.",
-                confirmButtonText: "Entendido",
-            });
-        }
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Espacios en blanco",
+            text: "Por favor revise los campos resaltados.",
+            confirmButtonText: "Entendido",
+        });
+    }
 }
 
-    function validarEspaciosVacios() {
-        let error = false;
-        if (nombre.value === "") {
-            nombre.classList.add("input-error");
-            error = true;
-        } else {
-            nombre.classList.remove("input-error");
-        }
-        if (apellido.value === "") {
-            apellido.classList.add("input-error");
-            error = true;
-        } else {
-            apellido.classList.remove("input-error");
-        }
-        if (nombreUsuario.value === "") {
-            nombreUsuario.classList.add("input-error");
-            error = true;
-        } else {
-            nombreUsuario.classList.remove("input-error");
-        }
-        if (contraseña.value === "") {
-            contraseña.classList.add("input-error");
-            error = true;
-        } else {
-            contraseña.classList.remove("input-error");
-        }
-        if (confirmarContraseña.value === "") {
-            confirmarContraseña.classList.add("input-error");
-            error = true;
-        } else {
-            confirmarContraseña.classList.remove("input-error");
-        }
-        if (fechaNacimiento.value === "") {
-            fechaNacimiento.classList.add("input-error");
-            error = true;
-        } else {
-            fechaNacimiento.classList.remove("input-error");
-        }
-        if (genero.value === "") {
-            genero.classList.add("input-error");
-            error = true;
-        } else {
-            genero.classList.remove("input-error");
-        }
-        if(contraseña.value !== confirmarContraseña.value){
-            contraseña.classList.add("input-error");
-            confirmarContraseña.classList.add("input-error");
-            error = true;
-        }else{
-            contraseña.classList.remove("input-error");
-            confirmarContraseña.classList.remove("input-error");
-        }
-        return error;
+function validarEspaciosVacios() {
+    let error = false;
+    if (nombre.value === "") {
+        nombre.classList.add("input-error");
+        error = true;
+    } else {
+        nombre.classList.remove("input-error");
+    }
+    if (apellido.value === "") {
+        apellido.classList.add("input-error");
+        error = true;
+    } else {
+        apellido.classList.remove("input-error");
+    }
+    if (nombreUsuario.value === "") {
+        nombreUsuario.classList.add("input-error");
+        error = true;
+    } else {
+        nombreUsuario.classList.remove("input-error");
+    }
+    if (contraseña.value === "") {
+        contraseña.classList.add("input-error");
+        error = true;
+    } else {
+        contraseña.classList.remove("input-error");
+    }
+    if (confirmarContraseña.value === "") {
+        confirmarContraseña.classList.add("input-error");
+        error = true;
+    } else {
+        confirmarContraseña.classList.remove("input-error");
+    }
+    if (fechaNacimiento.value === "") {
+        fechaNacimiento.classList.add("input-error");
+        error = true;
+    } else {
+        fechaNacimiento.classList.remove("input-error");
+    }
+    if (genero.value === "") {
+        genero.classList.add("input-error");
+        error = true;
+    } else {
+        genero.classList.remove("input-error");
     }
 
-    registrarBtn.addEventListener("click", validarFormulario);
+    return error;
+}
+
+registrarBtn.addEventListener("click", validarFormulario);
