41a8a1390d8d4e6cac0c184bfea55545d23b8f8c
ROBERTO ANDRES THOMAS TORRES <rtt2594@gmail.com>
2026-03-10 18:39:26 -0600

    new: esquema models

---
+++ Sistema de gestion de personal/backend/models/empleado.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Creacion del esquema
+const schemaEmpleado = new mongoose.Schema({
+    nombre: {
+        type: String,
+        require: true,
+        unique: false
+    },
+    correo: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    contrasenia: {
+        type: String,
+        require: true,
+    },
+    direccion: {
+        provincia: {
+            type: String,
+            require: true,
+        },
+        distrito: {
+            type: String,
+            require: true,
+        },
+        canton: {
+            type: String,
+            require: true,
+        },
+        ubicacion: {
+            type: String
+        }
+    }
+})
