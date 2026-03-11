85f7d870e1e843fe2af0b8c91e7d87f271d8fe83
FABIOLA MATAMOROS GOMEZ <fmatamorosg@ucenfotec.ac.cr>
2026-03-10 20:40:51 -0600

    [new] modelos  post get y rutas de empleados

---
+++ sistema de gestion de personal/backend/modelos/empleado.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Creación del esquema
+const schemaEmpleado = new mongoose.Schema({
+    nombre:{
+        type: String,
+        required: true,
+        unique: false
+    },
+    correo:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    contrasenia:{
+        type: String,
+        required: true,
+    },
+    direccion:{
+        provincia:{
+            type: String,
+            required: true,
+        },
+        distrito:{
+            type: String,
+            required: true,
+        }, 
+        canton:{
+            type: String,
+            required: true,
+        },
+        ubicacion:{
+            type: String
+        }
+    }
+});
+
+const Empleado = mongoose.model("Empleado", schemaEmpleado);
+module.exports = Empleado; // Exportar el modelo para utilizarlo en el backend
---
+++ sistema de gestion de personal/backend/routes/empleado.route.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router(); // Crear la señal 
+const Empleado = require("../modelos/empleado.model");
+
+//Rutas
+//POST: Crear/enviar un nuevo dato a la BD
+router.post("/", async(req, res) => {
+    const {nombre, correo, contrasenia, direccion} = req.body;
+
+    if(!nombre || !correo || !contrasenia || !direccion){
+        return res.status(400).json({mensajeError: "Todos los datos son obligatorios"});
+    }
+
+    // Validar los campos anidados de la dirección
+    const {provincia, canton, distrito} = direccion;
+    if (!provincia || !canton || !distrito){
+        return res.status(400).json({mensajeError: "La dirección debe incluir provincia, canton y distrito"});
+    }
+    
+    try{
+        const nuevoEmpleado = new Empleado({nombre, correo, contrasenia, direccion});
+        await nuevoEmpleado.save();
+        res.status(201).json(nuevoEmpleado); 
+    } catch(error){
+        res.status(400).json({mensajeError: error.message});
+    }
+});
+
+// GET: Solicitar los datos de los empleados a la BD
+router.get("/", async(req, res) =>{
+    try{
+        const empleados = await Empleado.find();
+        res.json(empleados);
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+});
+
+
+module.exports = router; // Exportar la señal para utilizarla en el backend
