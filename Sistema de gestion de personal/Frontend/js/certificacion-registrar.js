82e5a53436f37094df113e5ff138659fb26a28c5
NELSON GERARDO FERNANDEZ GONZALEZ <nfernandezg@ucenfotec.ac.cr>
2026-03-24 21:16:53 -0600

    [new] frontend y js para certificaciones listado y registro

---
+++ Sistema de gestion de personal/Frontend/js/certificacion-registrar.js
@@ -0,0 +1,59 @@
+const inputNombre = document.getElementById("txtNombre");
+const inputInstitucion = document.getElementById("txtInstitucion");
+const inputDescripcion = document.getElementById("txtDescripcion")
+const btnRegistrar = document.querySelector("#btnRegistrarCertificacion");
+
+const inputsRequeridos = document.querySelectorAll("input[required]"); //seleccionar todos los inputs obligatorios
+
+
+function validar(){
+    let error = false;
+    for(let i = 0; i < inputsRequeridos.length; i++){
+        if(inputsRequeridos[i].value === ""){
+            error = true;
+            // Optimizar: El mensaje de error debería indicar cuál es el input obligatorio que se debe completar
+            Swal.fire({
+                icon: "warning",
+                title: "No se puede agregar la certificación",
+                text: "Por favor complete todos los campos.",
+                confirmButtonText: "Aceptar"
+            });
+        }
+    }
+    if(error === false){
+        registrarCertificacion();
+    }
+}
+
+function registrarCertificacion(){
+    const datosCertificacion = {
+        nombre: inputNombre.value,
+        institucion: inputInstitucion.value,
+        descripcion: inputDescripcion.value
+    };
+    fetch("http://localhost:3000/certificaciones", {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify(datosCertificacion)
+    }).then(response => {
+        if(response.ok){ 
+            Swal.fire({
+                icon: "ok",
+                title: "Certificación registrada correctamente",
+                confirmButtonText: "Aceptar"
+            });
+        }
+        else{
+            Swal.fire({
+                icon: "error",
+                title: "No se puede registrar la certificación",
+                text: response,
+                confirmButtonText: "Aceptar"
+            });
+        }
+    });
+}
+
+btnRegistrar.addEventListener("click", validar);
